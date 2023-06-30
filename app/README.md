163b2a2d7aeecebbb9db43484051d46e51e09249
SabrinaAug <sabrina.a@ttc-testing.com>
2023-06-30 11:41:30 +0200

    Apprentissage de la modification d'un text en ajoutant de la couleur et en ajoutant un padding

---
+++ app/README.md
@@ -0,0 +1,14 @@
+# My Greeting Card
+
+Ce repertoire est créé comme une "Sandbox" pour m'apprendre à utiliser Android studio et développer des applications Android en Kotlin avec JetPack Compose
+
+Références : 
+    * [le site web / Cours de programmation :](https://developer.android.com/codelabs)
+
+## Installation Android Studio
+
+## Création de la première application Android (Greeting Card)
+
+
+
+
--- app/src/main/java/com/example/mygreetingcard/MainActivity.kt
+++ app/src/main/java/com/example/mygreetingcard/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mygreetingcard.ui.theme.MyGreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +35,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Cyan) {
         Text(
             text = "Hello $name!",
-            modifier = modifier
+            modifier = modifier.padding(24.dp)
         )
     }
 }
