9f9deaa2902d2242cc54ea41ab46d44515e98050
SabrinaAug <sabrina.a@ttc-testing.com>
2023-06-30 14:21:26 +0200

    Modification du text (changement de position et de taille)

--- app/src/main/java/com/example/mygreetingcard/MainActivity.kt
+++ app/src/main/java/com/example/mygreetingcard/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.mygreetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -11,9 +11,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mygreetingcard.ui.theme.MyGreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +22,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyGreetingCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Sabrina")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BirthdayGreetingWithText(
+                        message = "Happy Birthday Sammy!",
+                        from = "- Dean")
+
                 }
             }
         }
@@ -31,19 +37,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
+fun BirthdayGreetingWithText(
+    modifier: Modifier = Modifier,
+    message: String,
+    from: String
+){
+    Column {
         Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
+            text = message,
+            fontSize = 36.sp,
+            modifier = modifier.padding(15.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 15.sp,
+            modifier = modifier.padding(10.dp)
         )
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyGreetingCardTheme {
-        Greeting("Sabrina")
+        BirthdayGreetingWithText(
+            message = "Happy Birthday Sammy!",
+            from = "- Dean"
+        )
     }
 }
